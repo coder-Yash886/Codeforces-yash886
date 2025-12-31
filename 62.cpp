dbbf4e5c91507ee02f8911c8696abf8c7bb9f677
yash kumar <yashkumar.967565@gmail.com>
2026-01-01 00:43:07 +0530

    contest

---
+++ 62.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int>v(n);
+        set<int>s;
+        for(int i=0;i<n;i++){
+            cin>>v[i];
+            s.insert(v[i]);
+        }
+        sort(v.begin(),v.end());
+        for(int i=0;i<n;i++){
+            
+        }
+        
+
+    }
+}
---
+++ 63.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int x;
+    cin>>x;
+    int ans = 25-x;
+    cout<<ceil((double)ans/4)<<endl;
+
+}
